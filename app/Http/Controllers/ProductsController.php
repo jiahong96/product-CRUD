f9a4d558220524f52406f08a3adedffa5545f383
jiahong96 <jiahong9705@gmail.com>
2021-10-19 19:17:08 +0800

    feat(product): add API routes and controller

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the product.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $search = $request->input('search');
+
+        $query = Product::query();
+
+        if (isset($search)) {
+            $query = $query->whereNameLike($search);
+        }
+
+        return $query->paginate(10)->withQueryString();
+    }
+
+    /**
+     * Store a newly created product in storage.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Product::create($request->only([
+            'name', 'description', 'price', 'is_published',
+        ]));
+    }
+
+    /**
+     * Display the specified product.
+     *
+     * @param  Product $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    /**
+     * Update the specified product in storage.
+     *
+     * @param Request $request
+     * @param Product $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        $product->update($request->only([
+            'name', 'description', 'price', 'is_published',
+        ]));
+
+        return response()->json(['status' => 'Product updated successfully']);
+    }
+
+    /**
+     * Remove the specified product from storage.
+     *
+     * @param Product $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->destory();
+
+        return response()->json(['status' => 'Product deleted successfully'], 204);
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function scopeWhereNameLike(Builder $query, $value)
+    {
+        return $query->where('name', 'LIKE', "%{$value}%");
+    }
 }
